17bb67c527d926ebc20bdcb1eb60af9abccc784a
amit <amitmondoloishik@gmail.com>
2024-02-19 00:05:02 +0600

    coupon added

--- script.js
+++ script.js
@@ -16,33 +16,19 @@ let count4 = 0;
             setElementById("totalSeat",count);
             setElementById("sitCount",count2);
             setElementById("totalPrice",count3);
+
+            // update grand Total
+            grandTotal("NEW15");
     
             // add element
             const elementText = event.target.innerText;
             setBgColor(elementText);
-            console.log(elementText);
     
             // set dynamic element
-            const dynamicElement = document.getElementById("dynamicElemint");
-            
-            const div = document.createElement("div");
-    
-            const p1 = document.createElement("p");
-            p1.innerText = elementText;
-    
-            const p2 = document.createElement("p");
-            p2.innerText = "Ecomomic";
-    
-            const p3 = document.createElement("p");
-            p3.innerText = 5500;
-            
-            div.appendChild(p1);
-            div.appendChild(p2);
-            div.appendChild(p3);
-    
-            dynamicElement.appendChild(div);
-            dynamicElement.classList.add("dynamic-element");
-    
-            sit.disabled = true;
+            setDynamicElement(elementText);
+
+            // button disabled;
+            addDisabledClass(elementText);
+
         })
     }
--- utility.js
+++ utility.js
@@ -11,10 +11,42 @@ function setBgColor(id){
     const element = document.getElementById(id);
     element.style.background = "green";
     element.style.color = "white";
-    console.log(element);
-
 }
 
+function addDisabledClass(id){
+    document.getElementById(id).classList.add("btn-disabled");
+}
+
+function setDynamicElement(elementText){
+    const dynamicElement = document.getElementById("dynamicElement");
+    
+    const div = document.createElement("div");
+    const p1 = document.createElement("p");
+    p1.innerText = elementText;
+    const p2 = document.createElement("p");
+    p2.innerText = "Economic";
+    const p3 = document.createElement("p");
+    p3.innerText = 5500;
+    
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    dynamicElement.appendChild(div);
+    dynamicElement.classList.add("dynamic-element");
+    
+}
+
+function grandTotal(coupon){
+    const grandTotalInitial = getElementById("totalPrice");
+   if(coupon == "NEW15"){
+     const grandTotalFirst = grandTotalInitial * 0.85;
+     setElementById("grandTotal",grandTotalFirst);
+   }
+    if(coupon == "Cople 20"){
+     const grandTotalSecond = grandTotalInitial * 0.80;
+     setElementById("grandTotal",grandTotalSecond);
+    }
+}
 
 
 
