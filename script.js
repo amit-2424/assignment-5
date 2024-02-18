d81a83960eb7f879a428b925a31a2e1027798cd6
amit <amitmondoloishik@gmail.com>
2024-02-19 02:06:49 +0600

    max seat updated

--- script.js
+++ script.js
@@ -7,41 +7,53 @@ let count3 = 0;
 let count4 = 0;
 
 
-    for(const sit of AllBtn){
-        sit.addEventListener("click",function(event){
-            // count sit
+
+for (const sit of AllBtn) {
+    sit.addEventListener("click", function (event) {
+        // alert massage
+        if(count4 == 4){
+            alert("You parched max number seat");
+        }
+        if (count4 < 4) {
+            // count seat
             count = count - 1;
             count2 = count2 + 1;
             count3 = count3 + 5500;
-            setElementById("totalSeat",count);
-            setElementById("sitCount",count2);
-            setElementById("totalPrice",count3);
+            count4 = count4 + 1;
+            setElementById("totalSeat", count);
+            setElementById("sitCount", count2);
+            setElementById("totalPrice", count3);
 
-            // update grand Total
-            
-    
+
             // add element
             const elementText = event.target.innerText;
             setBgColor(elementText);
-    
+
             // set dynamic element
             setDynamicElement(elementText);
 
             // button disabled;
             addDisabledClass(elementText);
 
-        })
+        }
+
+    })
+}
+
+
+
+
+// Grand total update
+
+const couponBtn = document.getElementById("couponBtn");
+
+couponBtn.addEventListener("click", function (event) {
+    const couponInput = document.getElementById("couponInput").value;
+    if (couponInput === "NEW15") {
+        grandTotal(couponInput);
+        event.target.parentNode.remove(event.target);
+    } else if (couponInput === "Couple 20") {
+        grandTotal(couponInput);
+        event.target.parentNode.remove(event.target);
     }
-
-
-
-    const couponBtn = document.getElementById("couponBtn");
-
-     couponBtn.addEventListener("click",function(event){
-        const couponInput = document.getElementById("couponInput").value;
-         if(couponInput === "NEW15"){
-            grandTotal(couponInput);
-         }else if(couponInput === "Couple 20"){
-            grandTotal(couponInput);
-         }
-     });
+});
